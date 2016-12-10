0f4ac6a875486536936ca82ffc354fdee9ee452b
Marta Prałat <pralatmarta@gmail.com>
2016-12-10 23:04:13 +0100

    Hu samples added.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-
 from blob_detector import detect_blobs
 from getting_lines import get_staffs
 from photo_adjuster import adjust_photo
@@ -10,8 +9,9 @@ def main():
     adjusted_photo = adjust_photo(image)
     staffs = get_staffs(adjusted_photo)
     lines = staffs[0].get_lines_locations()
-    blobs = detect_blobs(adjusted_photo)
+
     classify_key(adjusted_photo, staffs[0])
+    blobs = detect_blobs(adjusted_photo)
 
     # TODO wykrywanie wysokosci nutek
 
